9c33b475420a16250e60880597095d84ed423f3c
lipkerton <petr.petuxof@yandex.ru>
2026-08-23 16:56:27 +0300

    frontend: src.App Header implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
   return (
-    <main>
-      <h1>3VService</h1>
-      <p>Современные цифровые решения для инженерии и производства.</p>
-    </main>
+    <>
+      <Header />
+      <main>
+        <h1>3VService</h1>
+        <p>Современные цифровые решения для инженерии и производства.</p>
+      </main>
+    </>
   )
 }
 
