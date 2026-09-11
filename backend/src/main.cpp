e9ba5c47d6660d902db1cdcc973a9df4065cd388
lipkerton <petr.petuxof@yandex.ru>
2026-09-11 14:15:38 +0300

    new addaptations

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -4,15 +4,18 @@
     #include <sys/socket.h>
     #include <netinet/in.h>
     #include <unistd.h>
+    #include <cerrno>
 #endif
 #include <iostream>
 
 #if defined(_WIN32)
     using sock_type = SOCKET;
-    SOCKET const invalid_socket = INVALID_SOCKET;
+    sock_type const invalid_socket = INVALID_SOCKET;
+    int const socket_error = SOCKET_ERROR;
 #elif defined(__linux__) || defined(__APPLE__)
     using sock_type = int;
-    int const invalid_socket = -1;
+    sock_type const invalid_socket = -1;
+    int const socket_error = -1;
 #endif
 
 int main() {
@@ -27,10 +30,14 @@ int main() {
 
     sock_type const listening_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (listening_socket == invalid_socket) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while initializing socket! Error code: " << wsa_error << "\n";
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while initializing socket! Error code: " << error_code << "\n";
         WSACleanup();
-        return wsa_error;
+        return error_code;
     }
     sockaddr_in server_address {};
     server_address.sin_family = AF_INET;
@@ -41,51 +48,75 @@ int main() {
         reinterpret_cast<sockaddr const*>(&server_address),
         sizeof(server_address)
     );
-    if (bind_result == SOCKET_ERROR) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while binding address to the socket! Error code: " << wsa_error << "\n";
+    if (bind_result == socket_error) {
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while binding address to the socket! Error code: " << error_code << "\n";
         closesocket(listening_socket);
         WSACleanup();
-        return wsa_error;
+        return error_code;
     }
     int const listen_result = listen(listening_socket, SOMAXCONN);
-    if (listen_result == SOCKET_ERROR) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while listening! Error code: " << wsa_error << "\n";
+    if (listen_result == socket_error) {
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while listening! Error code: " << error_code << "\n";
         closesocket(listening_socket);
         WSACleanup();
-        return wsa_error;
+        return error_code;
     }
     std::cout << "ThreeVServiceBackend is running!\n";
     sock_type const client_socket = accept(listening_socket, nullptr, nullptr);
     if (client_socket == invalid_socket) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while accepting client connection! Error code: " << wsa_error << "\n";
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while accepting client connection! Error code: " << error_code << "\n";
         closesocket(listening_socket);
         WSACleanup();
-        return wsa_error;
+        return error_code;
     }
     std::cout << "Client connected!\n";
     int const close_client_result = closesocket(client_socket);
-    if (close_client_result == SOCKET_ERROR) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while closing client socket! Error code: " << wsa_error << "\n";
+    if (close_client_result == socket_error) {
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while closing client socket! Error code: " << error_code << "\n";
         closesocket(listening_socket);
         WSACleanup();
-        return wsa_error;
+        return error_code;
     }
     int const close_listening_result = closesocket(listening_socket);
-    if (close_listening_result == SOCKET_ERROR) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while closing socket! Error code: " << wsa_error << "\n";
+    if (close_listening_result == socket_error) {
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while closing socket! Error code: " << error_code << "\n";
         WSACleanup();
-        return wsa_error;
+        return error_code;
     }
     int const cleanup_result = WSACleanup();
-    if (cleanup_result == SOCKET_ERROR) {
-        int const wsa_error = WSAGetLastError();
-        std::cerr << "Error while doing cleanup! Error code: " << wsa_error << "\n";
-        return wsa_error;
+    if (cleanup_result == socket_error) {
+        #if defined(_WIN32)
+            int const error_code = WSAGetLastError();
+        #elif defined(__linux__) || defined(__APPLE__)
+            int const error_code = errno;
+        #endif
+        std::cerr << "Error while doing cleanup! Error code: " << error_code << "\n";
+        return error_code;
     }
     return 0;
 }
