cb217339d5cf47cd5fde1417a131c18797611ba7
lipkerton <petr.petuxof@yandex.ru>
2026-09-12 14:23:09 +0300

    get_last_socket_error

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -54,11 +54,7 @@ int main() {
 
     sock_type const listening_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (listening_socket == invalid_socket) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while initializing socket! Error code: " << error_code << "\n";
         cleanup_networking();
         return error_code;
@@ -73,11 +69,7 @@ int main() {
         sizeof(server_address)
     );
     if (bind_result == socket_error) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while binding address to the socket! Error code: " << error_code << "\n";
         close_socket(listening_socket);
         cleanup_networking();
@@ -85,11 +77,7 @@ int main() {
     }
     int const listen_result = listen(listening_socket, SOMAXCONN);
     if (listen_result == socket_error) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while listening! Error code: " << error_code << "\n";
         close_socket(listening_socket);
         cleanup_networking();
@@ -98,11 +86,7 @@ int main() {
     std::cout << "ThreeVServiceBackend is running!\n";
     sock_type const client_socket = accept(listening_socket, nullptr, nullptr);
     if (client_socket == invalid_socket) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while accepting client connection! Error code: " << error_code << "\n";
         close_socket(listening_socket);
         cleanup_networking();
@@ -111,11 +95,7 @@ int main() {
     std::cout << "Client connected!\n";
     int const close_client_result = close_socket(client_socket);
     if (close_client_result == socket_error) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while closing client socket! Error code: " << error_code << "\n";
         close_socket(listening_socket);
         cleanup_networking();
@@ -123,22 +103,14 @@ int main() {
     }
     int const close_listening_result = close_socket(listening_socket);
     if (close_listening_result == socket_error) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while closing socket! Error code: " << error_code << "\n";
         cleanup_networking();
         return error_code;
     }
     int const cleanup_result = cleanup_networking();
     if (cleanup_result == socket_error) {
-        #if defined(_WIN32)
-            int const error_code = WSAGetLastError();
-        #elif defined(__linux__) || defined(__APPLE__)
-            int const error_code = errno;
-        #endif
+        int const error_code = get_last_socket_error();
         std::cerr << "Error while doing cleanup! Error code: " << error_code << "\n";
         return error_code;
     }
