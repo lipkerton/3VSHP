128556ed3508b590d804a18a5d4e6f812ef133e0
lipkerton <petr.petuxof@yandex.ru>
2026-09-14 23:07:28 +0300

    drogon server v0.1

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,5 +1,7 @@
 #include <drogon/drogon.h>
 
 int main() {
-    return 0;
+    drogon::HttpAppFramework& drogon_app = drogon::app();
+    drogon_app.addListener("127.0.0.1", 8080);
+    drogon_app.run();
 }
