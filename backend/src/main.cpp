49d8783f74e59884c35dce8d5f718348bbf00fb4
lipkerton <petr.petuxof@yandex.ru>
2026-09-16 08:03:29 +0300

    drogon ext

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -2,6 +2,19 @@
 
 int main() {
     drogon::HttpAppFramework& drogon_app = drogon::app();
+    drogon_app.registerHandler(
+        "/health",
+        [](
+            drogon::HttpRequestPtr const&, std::function<void(drogon::HttpResponsePtr const&)>&& callback
+        ){
+            drogon::HttpResponsePtr response = drogon::HttpResponse::newHttpResponse(
+                drogon::k200OK, drogon::CT_TEXT_PLAIN
+            );
+            response->setBody("OK");
+            callback(response);
+        },
+        {drogon::Get}
+    );
     drogon_app.addListener("127.0.0.1", 8080);
     drogon_app.run();
 }
