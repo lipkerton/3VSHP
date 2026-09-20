20e125465dffe8305a7acd32f7e72f4d9d62436a
lipkerton <petr.petuxof@yandex.ru>
2026-09-20 21:24:12 +0300

    HealthController

--- backend/src/interfaces/http/controllers/HealthController.cpp
+++ backend/src/interfaces/http/controllers/HealthController.cpp
@@ -1,1 +1,15 @@
+#include <drogon/drogon.h>
+
 #include "HealthController.h"
+
+
+void HealthController::asyncHandleHttpRequest(
+    drogon::HttpRequestPtr const&,
+    std::function<void(drogon::HttpResponsePtr const&)>&& callback
+) {
+    drogon::HttpResponsePtr response = drogon::HttpResponse::newHttpResponse(
+        drogon::k200OK, drogon::CT_TEXT_PLAIN
+    );
+    response->setBody("OK");
+    callback(response);
+}
