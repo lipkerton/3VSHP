ea3da7886e504da3282c8773e5777a757e54cf62
lipkerton <petr.petuxof@yandex.ru>
2026-09-22 22:44:54 +0300

    some new

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -4,8 +4,13 @@ int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "You need to specify config file!\n";
         return 1;
-    };
+    }
     drogon::HttpAppFramework& drogon_app = drogon::app();
-    drogon_app.loadConfigFile(argv[1]);
+    try {
+        drogon_app.loadConfigFile(argv[1]);
+    } 
+    catch(std::exception error) {
+        error.what();
+    }
     drogon_app.run();
 }
