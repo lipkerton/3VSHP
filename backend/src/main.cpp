e567404a224024f1e9044741a7abeb8d9cd955fd
lipkerton <petr.petuxof@yandex.ru>
2026-09-22 17:33:00 +0300

    command line args

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,7 +1,11 @@
 #include <drogon/drogon.h>
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "You need to specify config file!\n";
+        return 1;
+    };
     drogon::HttpAppFramework& drogon_app = drogon::app();
-    drogon_app.loadConfigFile("./config.json");
+    drogon_app.loadConfigFile(argv[1]);
     drogon_app.run();
 }
