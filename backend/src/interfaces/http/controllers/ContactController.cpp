0aa40fa36f996200f36f24a97ce92fcf5dac036e
lipkerton <petr.petuxof@yandex.ru>
2026-09-26 10:20:45 +0300

    small fix

--- backend/src/interfaces/http/controllers/ContactController.cpp
+++ backend/src/interfaces/http/controllers/ContactController.cpp
@@ -8,14 +8,15 @@ namespace three_v_service::interfaces::http {
         std::function<void(drogon::HttpResponsePtr const&)>&& callback
     ) {
         std::shared_ptr<Json::Value> const& request_json = request->getJsonObject();
-        Json::Value const& request_data = *request_json;
         Json::Value json_value = Json::Value(Json::objectValue);
         drogon::HttpResponsePtr response {};
         if (!request_json) {
             json_value["error"] = "invalid_json";
             response = drogon::HttpResponse::newHttpJsonResponse(json_value);
             response->setStatusCode(drogon::k400BadRequest);
-        } else if (
+        }
+        Json::Value const& request_data = *request_json;
+        if (
             !request_data.isObject() 
             || !request_data.isMember("email")
             || !request_data["email"].isString()
